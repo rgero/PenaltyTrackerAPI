127fca4e44271bcda44d1833538fe9535a99683d
Roy Gero <roy.gero.ii@gmail.com>
2019-02-02 20:38:06 -0500

    Start to implement the query parser. I need to think this through because a lot of parameters are going to be optional.

--- databasemanager.js
+++ databasemanager.js
@@ -13,9 +13,50 @@ class Database {
         })
     }
 
+    arrayParser(array, keyword){
+        var newString = ""
+        if (array){
+            if (array.length > 1){
+                for(var i = 0; i < array.length-1; i++)
+                {
+                    newString = newString + " " + keyword + " LIKE '%" + array[i] + "%' OR"
+                };
+            }
+            newString = newString + " " + keyword + " LIKE '%" + array[array.length-1] + "%'"
+        }
+        return newString
+    }
+
+    constructWhere(params){
+
+        // Default Values
+        var season = "Regular_18_19"
+        var players = ""
+        var sqlString = "SELECT * FROM "
+        
+        if (params["season"])
+        {
+            season = params["season"]
+        }
+        var sqlString = sqlString + season + " WHERE";
+        if (params["players"]){
+            sqlString = sqlString + this.arrayParser(params["players"], "playerName")
+        }
+        var sqlString = sqlString + " AND "
+        if (params["penalties"]){
+            sqlString = sqlString + this.arrayParser(params["penalties"], "penalty")
+        }
+
+        console.log(sqlString)
+        
+        return sqlString;
+
+    }
+
     all(params = {}){
         //Construct the Query Statement based on the Parameters, store in string
-        var sql = "SELECT * FROM Regular_18_19"
+        var sql = this.constructWhere(params);
+        console.log(sql);
 
         return new Promise((resolve, reject)=> {
             this.db.all(sql, (err, rows)=> {
