e39cd4a159aebbdfabb3676c55f358f69f9d35bd
Roy Gero <roy.gero.ii@gmail.com>
2019-02-20 10:47:08 -0500

    Adding the 'cors' module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 const QueryParser = require('./queryparser');
 const DatabaseManager = require('./databasemanager')
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 const database = new DatabaseManager('season.db');
 
