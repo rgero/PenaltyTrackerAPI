1172687e199cdead009a72bf88337a589ff399d4
Roy Gero <roy.gero.ii@gmail.com>
2023-07-31 22:51:30 -0400

    Fixed the test so it uses mocked data.

---
+++ tests/testData.js
@@ -0,0 +1,330 @@
+const testData = {
+    "copyright" : "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2023. All Rights Reserved.",
+    "totalItems" : 6,
+    "totalEvents" : 0,
+    "totalGames" : 6,
+    "totalMatches" : 0,
+    "metaData" : {
+      "timeStamp" : "20230801_024204"
+    },
+    "wait" : 10,
+    "dates" : [ {
+      "date" : "2022-02-22",
+      "totalItems" : 6,
+      "totalEvents" : 0,
+      "totalGames" : 6,
+      "totalMatches" : 0,
+      "games" : [ {
+        "gamePk" : 2021020511,
+        "link" : "/api/v1/game/2021020511/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T00:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 29,
+              "losses" : 18,
+              "ot" : 4,
+              "type" : "league"
+            },
+            "score" : 6,
+            "team" : {
+              "id" : 18,
+              "name" : "Nashville Predators",
+              "link" : "/api/v1/teams/18"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 35,
+              "losses" : 11,
+              "ot" : 5,
+              "type" : "league"
+            },
+            "score" : 4,
+            "team" : {
+              "id" : 13,
+              "name" : "Florida Panthers",
+              "link" : "/api/v1/teams/13"
+            }
+          }
+        },
+        "venue" : {
+          "id" : 5027,
+          "name" : "FLA Live Arena",
+          "link" : "/api/v1/venues/5027"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020511/content"
+        }
+      }, {
+        "gamePk" : 2021020528,
+        "link" : "/api/v1/game/2021020528/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T00:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 32,
+              "losses" : 14,
+              "ot" : 4,
+              "type" : "league"
+            },
+            "score" : 3,
+            "team" : {
+              "id" : 10,
+              "name" : "Toronto Maple Leafs",
+              "link" : "/api/v1/teams/10"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 26,
+              "losses" : 23,
+              "ot" : 1,
+              "type" : "league"
+            },
+            "score" : 4,
+            "team" : {
+              "id" : 29,
+              "name" : "Columbus Blue Jackets",
+              "link" : "/api/v1/teams/29"
+            }
+          }
+        },
+        "venue" : {
+          "id" : 5059,
+          "name" : "Nationwide Arena",
+          "link" : "/api/v1/venues/5059"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020528/content"
+        }
+      }, {
+        "gamePk" : 2021020584,
+        "link" : "/api/v1/game/2021020584/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T00:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 31,
+              "losses" : 14,
+              "ot" : 3,
+              "type" : "league"
+            },
+            "score" : 3,
+            "team" : {
+              "id" : 30,
+              "name" : "Minnesota Wild",
+              "link" : "/api/v1/teams/30"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 19,
+              "losses" : 26,
+              "ot" : 5,
+              "type" : "league"
+            },
+            "score" : 4,
+            "team" : {
+              "id" : 9,
+              "name" : "Ottawa Senators",
+              "link" : "/api/v1/teams/9"
+            }
+          }
+        },
+        "venue" : {
+          "id" : 5031,
+          "name" : "Canadian Tire Centre",
+          "link" : "/api/v1/venues/5031"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020584/content"
+        }
+      }, {
+        "gamePk" : 2021020848,
+        "link" : "/api/v1/game/2021020848/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T00:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 30,
+              "losses" : 14,
+              "ot" : 6,
+              "type" : "league"
+            },
+            "score" : 4,
+            "team" : {
+              "id" : 19,
+              "name" : "St. Louis Blues",
+              "link" : "/api/v1/teams/19"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 15,
+              "losses" : 26,
+              "ot" : 10,
+              "type" : "league"
+            },
+            "score" : 1,
+            "team" : {
+              "id" : 4,
+              "name" : "Philadelphia Flyers",
+              "link" : "/api/v1/teams/4"
+            }
+          }
+        },
+        "venue" : {
+          "id" : 5096,
+          "name" : "Wells Fargo Center",
+          "link" : "/api/v1/venues/5096"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020848/content"
+        }
+      }, {
+        "gamePk" : 2021020536,
+        "link" : "/api/v1/game/2021020536/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T03:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 22,
+              "losses" : 22,
+              "ot" : 6,
+              "type" : "league"
+            },
+            "score" : 3,
+            "team" : {
+              "id" : 28,
+              "name" : "San Jose Sharks",
+              "link" : "/api/v1/teams/28"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 25,
+              "losses" : 19,
+              "ot" : 9,
+              "type" : "league"
+            },
+            "score" : 4,
+            "team" : {
+              "id" : 24,
+              "name" : "Anaheim Ducks",
+              "link" : "/api/v1/teams/24"
+            }
+          }
+        },
+        "venue" : {
+          "id" : 5046,
+          "name" : "Honda Center",
+          "link" : "/api/v1/venues/5046"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020536/content"
+        }
+      }, {
+        "gamePk" : 2021020595,
+        "link" : "/api/v1/game/2021020595/feed/live",
+        "gameType" : "R",
+        "season" : "20212022",
+        "gameDate" : "2022-02-23T03:00:00Z",
+        "status" : {
+          "abstractGameState" : "Final",
+          "codedGameState" : "7",
+          "detailedState" : "Final",
+          "statusCode" : "7",
+          "startTimeTBD" : false
+        },
+        "teams" : {
+          "away" : {
+            "leagueRecord" : {
+              "wins" : 19,
+              "losses" : 20,
+              "ot" : 7,
+              "type" : "league"
+            },
+            "score" : 5,
+            "team" : {
+              "id" : 2,
+              "name" : "New York Islanders",
+              "link" : "/api/v1/teams/2"
+            }
+          },
+          "home" : {
+            "leagueRecord" : {
+              "wins" : 16,
+              "losses" : 33,
+              "ot" : 4,
+              "type" : "league"
+            },
+            "score" : 2,
+            "team" : {
+              "id" : 55,
+              "name" : "Seattle Kraken",
+              "link" : "/api/v1/teams/55"
+            }
+          }
+        },
+        "venue" : {
+          "name" : "Climate Pledge Arena",
+          "link" : "/api/v1/venues/null"
+        },
+        "content" : {
+          "link" : "/api/v1/game/2021020595/content"
+        }
+      } ],
+      "events" : [ ],
+      "matches" : [ ]
+    } ]
+}
+
+module.exports = {
+    testData: testData
+};
--- tests/utilities/PenaltyFetcher.test.js
+++ tests/utilities/PenaltyFetcher.test.js
@@ -1,10 +1,16 @@
 const PenaltyFetcher = require('../../src/utilities/PenaltyFetcher');
+const axios = require("axios");
+const {testData} = require('../testData');
+
+jest.mock("axios");
+
+beforeAll(() => {
+    axios.create.mockReturnThis();
+});
 
 test("Run Game URLS", async () => {
-  // This should be mocked in the future, but for now it's fine.
-  // I'm not happy with it but I need to investigate mocking axios properly.
   var penaltyFetcher = new PenaltyFetcher();
-  penaltyFetcher.setTargetDate("2022-02-22");
+  let mockedData = {data: testData};
 
   const expectedResult = ["/api/v1/game/2021020511/feed/live", 
                           "/api/v1/game/2021020528/feed/live", 
@@ -14,6 +20,7 @@ test("Run Game URLS", async () => {
                           "/api/v1/game/2021020595/feed/live"
                         ]
 
+  axios.get.mockResolvedValue(mockedData);
   await penaltyFetcher.FetchGameURLs();
   await expect(penaltyFetcher.gameURLs).toEqual(expectedResult);
 });
