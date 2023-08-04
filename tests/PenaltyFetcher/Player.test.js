71d5591fbdcbc782faa9360cc205c093a24762f1
Roy Gero <roy.gero.ii@gmail.com>
2023-08-04 10:59:29 -0400

    Adding tests for the Player Team.

--- tests/PenaltyFetcher/Player.test.js
+++ tests/PenaltyFetcher/Player.test.js
@@ -8,3 +8,18 @@ test("Constructs Valid Player URL", ()=> {
 
     expect(testPlayer.GetURL().toString()).toBe('http://nhl.com/player/taylor-hall-8475791')
 })
+
+describe("Player - Teams", ()=> {
+
+    test("Setting a valid team", ()=> {
+        testPlayer = new Player();
+        expect(()=>{testPlayer.setTeam("Boston Bruins")}).not.toThrow();
+        expect(testPlayer.team).toBe("Boston Bruins");
+    })
+
+    test("Setting invalid team", ()=> {
+        testPlayer = new Player();
+        expect(()=>{testPlayer.setTeam("Boston Red Sox")}).toThrow("Invalid Player Team");
+        expect(testPlayer.team).toBe("");
+    })
+})
